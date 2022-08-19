5ef42d642448e306a3b8bc24564e02925124f5bc
16leog <16leog@gmail.com>
2022-08-18 22:02:01 -0500

    formatted padding to have list borders come off edge

--- lib/screens/recipe_selection_screen.dart
+++ lib/screens/recipe_selection_screen.dart
@@ -10,10 +10,9 @@ class RecipeSelectionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: ListView(
         children: [
-          Padding(padding: EdgeInsets.all(24.5)),
+          Padding(padding: EdgeInsets.only(top: 49)),
           Text(
             "Coffee Recipes",
             key: Key("coffee-recipes-text"),
@@ -50,126 +49,130 @@ class RecipeList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: new BoxDecoration(
-          border: Border.all(
-            color: Color(0xff4C748B),
-            width: 3,
+    return Padding(
+      padding: const EdgeInsets.only(left: 19, top: 16, right: 19),
+      child: Container(
+          decoration: new BoxDecoration(
+            border: Border.all(
+              color: Color(0xff4C748B),
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
           ),
-          borderRadius: BorderRadius.circular(10.0),
-          
-        ),
-        child: Column(
-          children: [
-            
-            for (CoffeeRecipe recipe in recipes) 
-              Column(
-                children: [
-                 Container(                    
-                    height: 1,
-                    child: Divider(
-                      color: Color(0xFF4C748B),
-                      thickness: 1,
+          child: Column(
+            children: [
+              for (CoffeeRecipe recipe in recipes)
+                Column(
+                  children: [
+                    Container(
+                      height: 1,
+                      child: Divider(
+                        color: Color(0xFF4C748B),
+                        thickness: 1,
+                      ),
                     ),
-                  ),
-                  ListTile(
-                      title: Text(recipe.name,
-                          style: TextStyle(
-                              color: Color(0xff4C748B),
-                              fontWeight: FontWeight.w400,
-                              fontSize: 17)),
-                      trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
-                      
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RecipeDetailScreen(recipe)),
-                        );
-                      }),
-                ],
-              ),
-          ],
-        ));
-        
+                    ListTile(
+                        title: Text(recipe.name,
+                            style: TextStyle(
+                                color: Color(0xff4C748B),
+                                fontWeight: FontWeight.w400,
+                                fontSize: 17)),
+                        trailing:
+                            Icon(Icons.chevron_right, color: Color(0xff4C748B)),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    RecipeDetailScreen(recipe)),
+                          );
+                        }),
+                  ],
+                ),
+            ],
+          )),
+    );
   }
 }
 
 class ResourceList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: new BoxDecoration(
-          border: Border.all(
-            color: Color(0xff4C748B),
-            width: 3,
+    return Padding(
+      padding: const EdgeInsets.only(left: 19, top: 16, right: 19),
+      child: Container(
+          decoration: new BoxDecoration(
+            border: Border.all(
+              color: Color(0xff4C748B),
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
           ),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Column(
-          children: [
-            ListTile(
-              onTap: () => launch(
-                  'https://www.gourmetcoffees.com/?gclid=CjwKCAjwo_KXBhAaEiwA2RZ8hFEFqJHq246wm35UUSFNHXE53Gmintks5Q7QqzNn4_FhWxmNpdsNVhoCi0YQAvD_BwE'),
-              title: Text("Coffee",
-                  style: TextStyle(
-                      color: Color(0xff4C748B),
-                      fontWeight: FontWeight.w400,
-                      fontSize: 17)),
-              trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
-            ),
-             Container(                    
-                    height: 1,
-                    child: Divider(
-                      color: Color(0xFF4C748B),
-                      thickness: 1,
-                    ),
-                  ),
-            ListTile(
-              onTap: () => launch(
-                  'https://nymag.com/strategist/article/best-coffee-grinders.html'),
-              title: Text("Grinders",
-                  style: TextStyle(
-                      color: Color(0xff4C748B),
-                      fontWeight: FontWeight.w400,
-                      fontSize: 17)),
-              trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
-            ),
-             Container(                    
-                    height: 1,
-                    child: Divider(
-                      color: Color(0xFF4C748B),
-                      thickness: 1,
-                    ),
-                  ),
-            ListTile(
-              onTap: () =>
-                  launch('https://nypost.com/article/best-tea-kettles/'),
-              title: Text("Kettles",
-                  style: TextStyle(
-                      color: Color(0xff4C748B),
-                      fontWeight: FontWeight.w400,
-                      fontSize: 17)),
-              trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
-            ),
-             Container(                    
-                    height: 1,
-                    child: Divider(
-                      color: Color(0xFF4C748B),
-                      thickness: 1,
-                    ),
-                  ),
-            ListTile(
-              onTap: () => launch(
-                  'https://www.homegrounds.co/best-pour-over-coffee-makers/'),
-              title: Text("Homebrew Dripper",
-                  style: TextStyle(
-                      color: Color(0xff4C748B),
-                      fontWeight: FontWeight.w400,
-                      fontSize: 17)),
-              trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
-            ),
-          ],
-        ));
+          child: Column(
+            children: [
+              ListTile(
+                onTap: () => launch(
+                    'https://www.gourmetcoffees.com/?gclid=CjwKCAjwo_KXBhAaEiwA2RZ8hFEFqJHq246wm35UUSFNHXE53Gmintks5Q7QqzNn4_FhWxmNpdsNVhoCi0YQAvD_BwE'),
+                title: Text("Coffee",
+                    style: TextStyle(
+                        color: Color(0xff4C748B),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17)),
+                trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
+              ),
+              Container(
+                height: 1,
+                child: Divider(
+                  color: Color(0xFF4C748B),
+                  thickness: 1,
+                ),
+              ),
+              ListTile(
+                onTap: () => launch(
+                    'https://nymag.com/strategist/article/best-coffee-grinders.html'),
+                title: Text("Grinders",
+                    style: TextStyle(
+                        color: Color(0xff4C748B),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17)),
+                trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
+              ),
+              Container(
+                height: 1,
+                child: Divider(
+                  color: Color(0xFF4C748B),
+                  thickness: 1,
+                ),
+              ),
+              ListTile(
+                onTap: () =>
+                    launch('https://nypost.com/article/best-tea-kettles/'),
+                title: Text("Kettles",
+                    style: TextStyle(
+                        color: Color(0xff4C748B),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17)),
+                trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
+              ),
+              Container(
+                height: 1,
+                child: Divider(
+                  color: Color(0xFF4C748B),
+                  thickness: 1,
+                ),
+              ),
+              ListTile(
+                onTap: () => launch(
+                    'https://www.homegrounds.co/best-pour-over-coffee-makers/'),
+                title: Text("Homebrew Dripper",
+                    style: TextStyle(
+                        color: Color(0xff4C748B),
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17)),
+                trailing: Icon(Icons.chevron_right, color: Color(0xff4C748B)),
+              ),
+            ],
+          )),
+    );
   }
 }
